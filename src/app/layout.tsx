e306c5b98115c571428c8f8430abb02276467580
nong phloeut <nong.phloeut.ext@sodexo.com>
2026-07-20 09:54:02 +0700

    implement dynamic shipping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} disableTransitionOnChange>
+        <ThemeProvider>
           <LanguageProvider>{children}</LanguageProvider>
         </ThemeProvider>
       </body>
